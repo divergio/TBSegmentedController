e1d4e9a3cebfee3d152950a7571457cc02f6accf
Tyler Barth <divergio@gmail.com>
2013-01-30 00:41:00 +0800

    Initial Commit

---
+++ TBSegmentedController/TBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TBAppDelegate.h
+//  TBSegmentedController
+//
+//  Created by Tyler Barth on 2013-01-30.
+//  Copyright (c) 2013年 Tyler Barth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
