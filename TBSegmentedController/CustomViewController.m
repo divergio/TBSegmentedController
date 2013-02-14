b331d2c6b252effe9fd8f58ffc55f680a4804f65
Tyler Barth <divergio@gmail.com>
2013-02-14 18:12:54 +0800

    Added examples of hints and titles

--- TBSegmentedController/CustomViewController.m
+++ TBSegmentedController/CustomViewController.m
@@ -11,9 +11,12 @@
 
 @interface CustomViewController () <TBSegmentedControlDelegate>
 
-@property (nonatomic, strong) IBOutlet UILabel* label;
+@property (nonatomic, strong) IBOutlet UILabel* label0;
+@property (nonatomic, strong) IBOutlet UILabel* label1;
 
-@property (nonatomic, strong) IBOutlet TBSegmentedControl* control;
+@property (nonatomic, strong) IBOutlet TBSegmentedControl* control0;
+@property (nonatomic, strong) IBOutlet TBSegmentedControl* control1;
+
 
 @end
 
@@ -21,19 +24,53 @@
 
 - (NSArray*) imagesForSegmentButton:(TBSegmentedControl *)view
 {
-    return [NSArray arrayWithObjects:[UIImage imageNamed:@"segment_0_selected.png"],
-            [UIImage imageNamed:@"segment_1_selected.png"],
-            [UIImage imageNamed:@"segment_2_selected.png"], nil];
+    if (view == self.control0) {
+        return [NSArray arrayWithObjects:[UIImage imageNamed:@"segment_0_selected.png"],
+                [UIImage imageNamed:@"segment_1_selected.png"],
+                [UIImage imageNamed:@"segment_2_selected.png"], nil];
+    } else if (view == self.control1) {
+        return [NSArray arrayWithObjects:[UIImage imageNamed:@"segment_0_selected.png"],
+                [UIImage imageNamed:@"segment_1_selected.png"],
+                [UIImage imageNamed:@"segment_2_selected.png"], nil];
+    }
+    else
+#ifdef DEBUG
+        NSLog(@"Invalid segment control");
+#endif
+    return nil;
 }
+
+- (NSArray*) titlesForSegmentButton:(TBSegmentedControl *)view
+{
+    if (view == self.control1) {
+        return @[ @"First", @"Second", @"Third"];
+    }
+    //No labels for the other controls
+    return nil;
+}
+
+- (NSArray*) accessabilityHintForSegmentButton:(TBSegmentedControl *)view
+{
+    if (view == self.control0) {
+        return @[ @"Number 1", @"Number 2", @"Number 3"];
+    }
+    else if (view == self.control1) {
+        return @[ @"does something", @"something else", @"a third option"];
+    }
+    
+    return nil;
+}
+
 
 - (void) viewWillAppear:(BOOL)animated
 {
     //Just to initialize the label
-    [self didChangeState:self.control];
+    [self didChangeState];
 }
-- (void) didChangeState:(TBSegmentedControl *)view
+- (void) didChangeState
 {
-    self.label.text = [NSString stringWithFormat:@"Selected = %i",view.selectedIndex + 1];
+    self.label0.text = [NSString stringWithFormat:@"Selected = %i",self.control0.selectedIndex + 1];
+    self.label1.text = [NSString stringWithFormat:@"Selected = %i",self.control1.selectedIndex + 1];
 }
 
 @end
