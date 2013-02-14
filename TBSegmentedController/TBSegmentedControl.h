266454ca3cde8253de818ec8176190677bd658b5
Tyler Barth <divergio@gmail.com>
2013-02-14 18:12:40 +0800

    Add basic accessibility support and label support

--- TBSegmentedController/TBSegmentedControl.h
+++ TBSegmentedController/TBSegmentedControl.h
@@ -3,21 +3,30 @@
 //
 //  Created by Tyler Barth on 2013-01-29.
 
-
 #import <UIKit/UIKit.h>
 
 @class TBSegmentedControl;
 
 @protocol TBSegmentedControlDelegate <NSObject>
 
-- (void) didChangeState: (TBSegmentedControl*) view;
-
 //Number of segments is determined implicitly by the number of images
 //One image for each button selected
+@required
 - (NSArray*) imagesForSegmentButton: (TBSegmentedControl*) view;
 
-//Labels 
-- (NSArray*) textForSegmentButton: (TBSegmentedControl*) view;
+@optional
+//Labels, array of NSString*
+// If you require more complicated settings, like title color or format
+// use the UIAppearance protocol and change UIButton when in class
+// TBSegmented control
+- (NSArray*) titlesForSegmentButton:(TBSegmentedControl*)view;
+
+//Optional accessibility hints
+// Array of NSStrings (should be NSLocalizedStrings)
+- (NSArray*) accessabilityHintForSegmentButton:(TBSegmentedControl*)view;
+
+- (void) didChangeState: (TBSegmentedControl*) view;
+
 
 @end
 
--- TBSegmentedController/TBSegmentedControl.m
+++ TBSegmentedController/TBSegmentedControl.m
@@ -11,6 +11,7 @@
 
 @property (nonatomic, strong) NSArray* images;
 @property (nonatomic, strong) NSNumber* numberOfSegments;
+@property (nonatomic, strong) NSMutableArray* buttons;
 
 @end
 
@@ -26,13 +27,19 @@ static char CW_BUTTON_KEY;
     }
     return self;
 }
+
+- (NSArray*) images
+{
+    //Lazy load images
+    if (!_images) {
+        _images = [self.delegate imagesForSegmentButton:self];
+    }
+    return _images;
+}
             
 - (void) awakeFromNib
 {
     [super awakeFromNib];
-
-    //Get images to use for the button
-    self.images = [self.delegate imagesForSegmentButton:self];
     
     //Determine the number of segments to the control implicitly
     //Based on the number of selected images passed int
@@ -45,13 +52,26 @@ static char CW_BUTTON_KEY;
     //Or even use resizable images,
     //But we're just going to assume the images are custom made for these buttons.
     CGSize size = [(UIImage*)[self.images objectAtIndex:0] size];
-        
+    
+    NSArray *titles = nil;
+    if ([self.delegate respondsToSelector:@selector(titlesForSegmentButton:)]) {
+        titles = [self.delegate titlesForSegmentButton:self];
+    }
+    
+    NSArray *accessibilityHints = nil;
+    if ([self.delegate respondsToSelector:@selector(accessabilityHintForSegmentButton:)]) {
+        accessibilityHints = [self.delegate accessabilityHintForSegmentButton:self];
+    }
+    
     //Set frame according to passed in image.
     [self setFrame:CGRectMake(self.frame.origin.x, self.frame.origin.y, size.width, size.height)];
     
     CGFloat increment = size.width / [self.numberOfSegments floatValue];
     
     //Make buttons for the sections
+    
+    //This is just an array to help keep track of the buttons
+    self.buttons = [[NSMutableArray alloc] initWithCapacity:[self.images count]];
     
     for (int i = 0; i < [self.numberOfSegments integerValue]; ++i) {
         UIButton* button = [UIButton buttonWithType:UIButtonTypeCustom];
@@ -64,7 +84,13 @@ static char CW_BUTTON_KEY;
         
         [button addTarget:self action:@selector(changeState:) forControlEvents:UIControlEventTouchDown];
         
+        [button setTitle:titles[i] forState:UIControlStateNormal];
+        [button setAccessibilityHint:accessibilityHints[i]];
+        
+        //Put it in the correct container
+        
         [self addSubview:button];
+        [self.buttons addObject:button];
     }
     
     //Initialize with segment 0 selected
@@ -74,6 +100,9 @@ static char CW_BUTTON_KEY;
 //Setting the selected segment makes the buttons appearance change. 
 - (void) setSelectedIndex:(NSUInteger)selectedIndex
 {
+    //Make the button selected for accessibility
+    [(UIButton*) self.buttons[selectedIndex] setSelected:YES];
+    
     _selectedIndex = selectedIndex;
     [self setImage:[self.images objectAtIndex:selectedIndex]];
     
@@ -87,7 +116,31 @@ static char CW_BUTTON_KEY;
     self.selectedIndex = [index integerValue];
     
     //Inform the delegate.
-    [self.delegate didChangeState:self];
+    if ([self.delegate respondsToSelector:@selector(didChangeState:)]) {
+        [self.delegate didChangeState:self];
+    }
 }
+
+
+#pragma mark - UIAccessibilityContainer protocol 
+- (NSInteger)accessibilityElementCount
+{
+    return [self.images count];
+}
+
+- (id)accessibilityElementAtIndex:(NSInteger)index
+{
+    return self.buttons[index];
+}
+
+- (NSInteger)indexOfAccessibilityElement:(id)element
+{
+    NSNumber* index = [NSNumber numberWithInt:0];
+    
+    index = (NSNumber*) objc_getAssociatedObject(element, &CW_BUTTON_KEY);
+    
+    return [index integerValue] ;
+}
+
 
 @end
